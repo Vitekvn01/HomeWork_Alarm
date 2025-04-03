ee9b78372577b726875939f3a8ca2af99db86cef
vitekvn01@yandex.ru <vitekvn01@yandex.ru>
2025-04-03 17:36:47 +0700

    Upd

--- Assets/Original/Scripts/Criminal.cs
+++ Assets/Original/Scripts/Criminal.cs
@@ -3,9 +3,26 @@ using UnityEngine;
 
 public class Criminal : MonoBehaviour
 {
-    [SerializeField] private float _speed; 
+    [SerializeField] private float _speed;
+
+    private Animator _animator;
+
+    private void OnValidate()
+    {
+        if (_speed < 0)
+        {
+            _speed = 0;
+        }
+    }
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
     private void Update()
     {
+        _animator.SetFloat("Speed", _speed);
         transform.position += transform.forward * (_speed * Time.deltaTime);
     }
 }
